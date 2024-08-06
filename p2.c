9a44a77f0d1e229b9fa5854d4ca8ccb8e4c73909
Abhiram Kanna Seelamanthula <88733940+aksisonline@users.noreply.github.com>
2024-08-06 04:13:59 +0000

    second

--- p2.c
+++ p2.c
@@ -1,29 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
 void reverseWords(char* str) {
-    int start = 0;
     int end = 0;
     int length = strlen(str);
 
-    // Remove commas and periods from the string
     for (int i = 0; i < length; i++) {
-        if (str[i] == ',' || str[i] == '.') {
-            continue;
+        if (str[i] == ',' || str[i] == '.' || str[i] == '\n')
+        {
+            str[i] = ' ';
         }
         str[end++] = str[i];
     }
-    str[end] = '\0'; // Add null terminator at the end
+    str[end] = '\0'; 
 
-    // Reverse the words in the modified string
-    while (end <= length) {
-        if (str[end] == ' ' || str[end] == '\0') {
-            int i;
-            for (i = end - 1; i >= start; i--) {
-                printf("%c", str[i]);
-            }
-            if (str[end] != '\0') {
-                printf("%c", str[end]);
-            }
-            start = end + 1;
+    // Reverse the characters of each word in the string
+    char* word = strtok(str, " ");
+    char* words[1000];
+    int count = 0;
+
+    while (word != NULL) {
+        int wordLength = strlen(word);
+        for (int i = 0, j = wordLength - 1; i < j; i++, j--) {
+            char temp = word[i];
+            word[i] = word[j];
+            word[j] = temp;
         }
-        end++;
+        words[count++] = word;
+        word = strtok(NULL, " ");
     }
+
+    for (int i = 0; i < count; i++) {
+        printf("%s ", words[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    char str[1000];
+    printf("Enter a long string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Reversed words: ");
+    reverseWords(str);
+
+    return 0;
 }
